4630d0e0c0a20f782ead5e4c79399136f7a792a9
Htet Ko Ko <htetkokoemail@gmail.com>
2024-08-27 01:48:04 +0630

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function toggleDrawer(){
 }
 $(document).ready(function(){
   /*Video Json*/
-  const jsonFile = '/data.json'
+  const jsonFile = 'https://pronwebmm.vercel.app/data.json'
   var pos = 10;
   var posEnd;
   $('#seebtn').click(function(){
