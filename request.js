d02f0fdf3099edfcefbb5a8ae15c72d521125343
Htet Ko Ko <htetkokoemail@gmail.com>
2024-08-28 15:42:22 +0630

    Update request.js

--- request.js
+++ request.js
@@ -5,21 +5,20 @@ function direct(url) {
 }
 /* Request Video */
 function requestVideo(api,s,e) {
-    var videoView = ''
     $.get(api)
     .done(function(data){
       for(i=s;i<=e;i++){
         let video = direct(data[i].link)
         let photo = data[i].image;
         let name = data[i].title;
-        videoView = videoView + `<div class="bg-secondary border border-1 border-dark p-2 pt-0 mb-4 shadow" id="vd">
+        let videoView = `<div class="bg-secondary border border-1 border-dark p-2 pt-0 mb-4 shadow" id="vd">
       <video src="${video}" controls width="100%" height="250px" class="mb-0 mt-1" poster="${photo}"></video>
       <div class="text-light  p-3  rounded m-0">${name}</div>
     </div>`;
+          $('#vdView').append(videoView);
       }
       $('#more').show();
       $('#list').text(data.length - e - 7);
-      $('#vdView').html(videoView);
     })
     .fail(function(xhr, status, error){
       alert('Sorry : Error404//contact us => (https://htetkoko.info)')
