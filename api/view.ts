19cf66625fce13353e429b455938d597742b5fc6
Htet Ko Ko <htetkokoemail@gmail.com>
2024-09-26 12:11:02 +0630

    Update and rename view.js to view.ts

--- api/view.ts
+++ api/view.ts
@@ -2,7 +2,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { video,title } = req.query
-  return res.json({
-    message: title,
-  })
+  return res.send(`
+    <h1>${title}</h1>
+  `)
 }
