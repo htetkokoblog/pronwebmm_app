87eb9c7d9454871c675fe2555e058b1d6fb53ccb
Htet Ko Ko <htetkokoemail@gmail.com>
2024-09-27 01:15:04 +0630

    Update request.js

--- script/request.js
+++ script/request.js
@@ -9,11 +9,11 @@ function requestVideo(api, s, e) {
         .done(function(data) {
             var videos = ''
             for (i = s; i <= e; i++) {
-                let videoUrl = direct(data[i].link)
+                let videoUrl = data[i].link
                 let photo = data[i].image;
                 let name = data[i].title;
                 videos = `${videos}<div class="bg-secondary border border-1 border-dark p-2 pt-0 mb-4 shadow" id="vd">
-      <video src="${videoUrl}" controls width="100%" height="250px" class="mb-0 mt-1" poster="${photo}"></video>
+      <video src="https://htetkoko.vercel.app/api/mediafire?url=${videoUrl}" controls width="100%" height="250px" class="mb-0 mt-1" poster="${photo}"></video>
       <div class="text-light  p-3  rounded m-0">${name}</div>
     </div>`;
             }
